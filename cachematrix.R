1eacc53907ff612bcc7dfd813f4118a7d6b4abed
Datum 2020 <sen.anandamoy@gmail.com>
2014-08-24 16:29:49 -0700

    Update README as specified
    solve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This routine takes as input a matrix from the user
+## and creates a special object that can optimize matrix
+## inversion by caching results.
+## Example: sqmar
+## sqmat <- matrix(4:7,2,2)
+## cachedmat <- makeCacheMatrix(sqmat)
+## cacheSolve(cm)
 
-## Write a short comment describing this function
-
+## This routine takes a matrix as input and exposes 
+## routines which allow cacheSolve to fetch cached
+## inverted matrix copy, if it already exists.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
 }
 
-
-## Write a short comment describing this function
+## This routine takes as input a special "cachedMatrix"
+## and returns a inverted one
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached matrix inversion data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
